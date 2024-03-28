0199b4743232443aceca33bc160032fe185a0dcd
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-28 22:12:18 +0530

    routes are created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 // components
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home.jsx';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import About from './components/About/About.jsx';
+import Projects from './components/Projects/Projects.jsx';
+import Contact from './components/Contacts/Contact.jsx';
+import Login from './components/Login/Login.jsx';
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/account" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
