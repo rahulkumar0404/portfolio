98d7021c0046f78462a32f5586bb3a32316365e5
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-28 22:11:48 +0530

    all imports are collected

---
+++ frontend/src/constants.js
@@ -0,0 +1,29 @@
+import cssImage from './Images/css.svg';
+import cursorImage from './Images/cursor.png';
+import expressImage from './Images/express.svg';
+import htmlImage from './Images/html.svg';
+import javascriptImage from './Images/javascript.svg';
+import moonImage from './Images/moon.jpg';
+import nodeJsImage from './Images/nodejs.svg';
+import profileImage from './Images/profileImage.png';
+import reactImage from './Images/React.png';
+import spaceImage from './Images/space.jpg';
+import ticTacToeImage from './Images/ticTacToe.png';
+import venusImage from './Images/venus.jpg';
+import mongodbImage from './Images/mongodb.jpg'
+
+export {
+  cssImage,
+  cursorImage,
+  expressImage,
+  htmlImage,
+  javascriptImage,
+  moonImage,
+  nodeJsImage,
+  profileImage,
+  reactImage,
+  spaceImage,
+  ticTacToeImage,
+  venusImage,
+  mongodbImage,
+};
