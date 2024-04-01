85867ba179095840d97a0ec94a7b8e02fe9493ef
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-01 18:14:32 +0530

    update user added

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './reducers/user';
 import loginSlice from './reducers/login';
+import updateUserSlice from './reducers/updateUser';
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     login: loginSlice.reducer,
+    updateUser: updateUserSlice.reducer,
   },
 });
 
